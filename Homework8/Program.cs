c1990c3e6b30cac3d9d38a73ed14c43b36c596ce
miroslav-slavchev <miroslav.svetoslavov.slavchev@gmail.com>
2022-05-15 13:15:42 +0300

    implement homework8

---
+++ Homework8/Program.cs
@@ -0,0 +1,55 @@
+﻿using Homework8.SchoolComponents;
+using Homework8.Utils;
+using System;
+using System.Collections.Generic;
+
+namespace Homework8
+{
+    internal class Program
+    {
+        private const string StudentsTextFile = "Students.txt";
+
+        static void Main(string[] args)
+        {
+            School school = new School("Sofia School", "Sofia, 1000, Center");
+
+            string[] textLines = TextFilereader.GetFileTextContentLines(StudentsTextFile);
+            List<Student> students = new();
+
+            for (int i = 0; i < textLines.Length; i++)
+            {
+                Student person = GetPersonDataFromLine(i, textLines[i]);
+                school.AddStudent(person);
+            }
+
+            school.GetExcelentStudents().ForEach(student => student.Speak());
+        }
+
+        private static Student GetPersonDataFromLine(int id, string textData)
+        {
+            string[] data = textData.Split(":");
+            string[] baseData = data[0].Split(",");
+            string marksData = data[1];
+
+            string name = baseData[0];
+            int age = int.Parse(baseData[1]);
+            Dictionary<string, int> marks = GetMarksDictionary(marksData);
+
+            return new Student(id, name, age, marks);
+        }
+
+        private static Dictionary<string, int> GetMarksDictionary(string marksData)
+        {
+            Dictionary<string, int> marks = new Dictionary<string, int>();
+     
+            string[] subjectPairs = marksData.Split(",");
+            foreach (var subjectPair in subjectPairs)
+            {
+                string[] subjectData = subjectPair.Split("-");
+                marks.Add(subjectData[0], int.Parse(subjectData[1]));
+            }
+
+            return marks;
+        }
+    }
+}
---
+++ Homework8/Utils/TextFileReader.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework8.Utils
+{
+    /// <summary>
+    /// Static class that manages the json files in the Directory.
+    /// </summary>
+    internal static class TextFilereader
+    {
+        /// <summary>
+        /// Directory name.
+        /// </summary>
+        public static string DirectoryName { get; set; } = "StudentsData";
+
+        /// <summary>
+        /// Gets the file text content lines.
+        /// </summary>
+        /// <param name="fileName">The name of the file (including the extension.</param>
+        public static string[] GetFileTextContentLines(string fileName) => File.ReadAllLines(GetFileAbsolutePath(fileName));
+
+        /// <summary>
+        /// Gets the file text content.
+        /// </summary>
+        /// <param name="fileName">The name of the file (including the extension.</param>
+        public static string GetFileTextContent(string fileName) => File.ReadAllText(GetFileAbsolutePath(fileName));
+
+        private static string GetFileAbsolutePath(string fileName)
+        {
+            string directoryPath = GetDirectoryPath(DirectoryName);
+            string filePath = $"{directoryPath}\\{fileName}";
+            return filePath;
+        }
+
+        private static string GetDirectoryPath(string directoryName)
+        {
+            string relativePath = $"\\{directoryName}";
+            string baseDirPath = AppDomain.CurrentDomain.BaseDirectory;
+            baseDirPath = baseDirPath.Replace("\\bin\\Debug\\net5.0\\", "");
+            string absolutePath = baseDirPath + relativePath;
+            return absolutePath;
+        }
+    }
+}
